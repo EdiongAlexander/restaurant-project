7a7a58c90cc525d10ef8d7d29507aa657a584618
KaraEno <abasiakaraalexander@gmail.com>
2022-07-24 21:29:56 +0100

    Restaurant project

--- src/Component/Navigation/layout.js
+++ src/Component/Navigation/layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Footer from './footer/footer'
-// import Sidebar from './sidebar'
+
 
 const Layout = (props)=>{
     return(
         <div>
-            {/* <Sidebar /> */}
+            
             <main>
+                
                 {props.children}
             </main>
             <Footer />
